ccea55bc053ecbff8d559c12aefc432665bd5d84
Chi Trung <trunghcw@gmail.com>
2023-05-17 17:22:38 +0700

    exercise 8 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation';
-import Player from './Players';
 import Footer from './Footer';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className='App'>
       <Navigation />
-      <Player />
+      <Main />
       <Footer />
     </div>
   );
